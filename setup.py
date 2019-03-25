2ef4d209cba4795b88472575053aa41c7064ef35
João Victor Monte Raposo Tavares <jotavemonte@gmail.com>
2019-03-25 11:12:20 -0300

    + setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jotavemonte",
+    version="0.0.1",
+    author="João Victor Monte R Tavares",
+    author_email="jotavmeonte@gmail.com",
+    description="DEA - python interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jotavemonte/ezdea",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
